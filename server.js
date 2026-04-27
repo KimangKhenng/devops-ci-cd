0136469de5043fa0aa6f0405eb1b94db262b4f8d
Kim Ang Kheang <kimangkheang@gmail.com>
2026-04-27 21:35:57 +0700

    GitHub Action Fix1

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/calculate', (req, res) => {
 
 // Users API (mock data)
 const users = [
-    { id: 1, name: 'Alices', email: 'alice@example.com' },
+    { id: 1, name: 'Alice', email: 'alice@example.com' },
     { id: 2, name: 'Bob', email: 'bob@example.com' }
 ];
 
